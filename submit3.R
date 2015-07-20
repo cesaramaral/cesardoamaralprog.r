5dba87d3931898a74ceddacf0034c20e053129b1
cesar do amaral <amaral12342012@hotmail.com>
2015-07-20 09:04:08 -0300

    submit3.R

---
+++ submit3.R
@@ -0,0 +1,28 @@
+rankhospital <- function(state, outcome, num = "best") {
+  
+  ## Lê dados de resultados
+  file_data <- read.csv("outcome-of-care-measures.csv", sep = ",")
+  
+  ## Verifica que o estado e os resultados são válidos
+  valid_states <- c("AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY")
+  valid_outcomes <- c("heart attack", "heart failure", "pneumonia")
+  if (!is.element(state, valid_states)) stop("invalid state")
+  if (!is.element(outcome, valid_outcomes)) stop("invalid outcome")
+  
+  ## Retorna o nome do hospital em que o estado com menor morte em 30 dias
+  data <- file_data[file_data$State == state,]
+  header_name <- NULL
+  if (outcome == "heart attack") header_name <- "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
+  else if (outcome == "heart failure") header_name <- "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"
+  else header_name <- "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
+  
+  sorted_data <- data[order(as.numeric(as.character(data[,header_name])), as.character(data[,"Hospital.Name"])),]
+  sorted_data <- sorted_data[!sorted_data[,header_name] == "Not Available",]
+  if (num == "best") {
+    return(best(state, outcome))
+  } else if (num == "worst") {
+    return(tail(as.character(sorted_data[,"Hospital.Name"]), n = 1))
+  }
+  return(as.character(sorted_data[,"Hospital.Name"][num]))
+}
+
